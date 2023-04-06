21b2079551c8a7f7a8a258330c0a9f2e3ab5a047
Wesley Prado <wesleypradobjj@gmail.com>
2023-04-06 23:52:13 +0400

    Design finalizado.

--- desafioSaoFrancisco/ContentView.swift
+++ desafioSaoFrancisco/ContentView.swift
@@ -39,6 +39,7 @@ struct ContentView: View {
                 }
             }
         }
+        .tint(.yellow)
     }
     
     
--- desafioSaoFrancisco/SelectorHatView.swift
+++ desafioSaoFrancisco/SelectorHatView.swift
@@ -7,74 +7,90 @@
 
 import SwiftUI
 
-//enum HogwartsCases {
-//    case notSelected
-//    case gryffindor
-//    case ravenclaw
-//    case hufflepuff
-//    case slytherin
-//}
 
 struct SelectorHatView: View {
     @State var counter = 0
-    @State var hatText: String? = String("Humm... Let me think...")
-    @State var houseView: Image? = Image("hogwarts")
+    @State var selectorHatText = "Hummm... Let me think..."
+    @State var selectorHatImage = "casashp"
     
     var body: some View {
         ZStack{
             
             SelectorHatBackgroundView()
-            
-            VStack {
-                
-                Text("Touch the Sorting Hat and let him decide your future!")
-                    .bold()
-                    .foregroundColor(Color("gold"))
-                    .font(.custom("Harry P", size: 49))
-                    .multilineTextAlignment(.center)
-                
-                
-                Button(action: {
-                    if counter == 0 {
-                        self.hatText = String("Gryffindor!!!")
-                        self.houseView = Image("grifinoria")
-                    }else if counter == 1 {
-                        self.hatText = String("Ravenclaw!!!")
-                        self.houseView = Image("corvinal")
-                    }else if counter == 2 {
-                        self.hatText = String("Hufflepuff!!!")
-                        self.houseView = Image("lufalufa")
-                    }else if counter == 3{
-                        self.hatText = String("Slytherin!!!")
-                        self.houseView = Image("sonserina")
-                        counter = 0
-                        return
+            Group{
+                VStack {
+                    
+                    Text("Touch the Sorting Hat and let him decide your future!")
+                        .bold()
+                        .foregroundColor(Color("gold"))
+                        .font(.custom("Harry P", size: 49))
+                        .multilineTextAlignment(.center)
+                    
+                    
+                    Button(action: {
+                        hatAction()
+                    }, label: {
+                        Image("chapeuSeletor")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 250, height: 250, alignment: .center)
+                        
+                    })
+                    
+                    VStack{
+                        Text(selectorHatText)
+                            .bold()
+                            .foregroundColor(Color.brown)
+                            .font(.custom("Harry P", size: 35))
+                            .multilineTextAlignment(.center)
+                        Image(selectorHatImage)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 340, height: 280, alignment: .center)
                     }
                     
-                    if counter < 3 {
-                        counter += 1
-                    }else {
-                        counter = 0
-                    }
-                }, label: {
-                    Image("chapeuSeletor")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 250, height: 250, alignment: .center)
                     
-                })
-                
-                //hatText!
-                    //.bold()
-                    //.foregroundColor(Color("gold"))
-                    //.font(.custom("Harry P", size: 20))
-                    //.multilineTextAlignment(.center)
-                
-                houseView!
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 280, height: 280, alignment: .center)
+                }
             }
+            
+        }
+    }
+    
+    func hatAction() {
+        if counter == 0 {
+            selectorHatText = "Gryffindor!!!"
+            selectorHatImage = "grifinoria"
+        }else if counter == 1 {
+            selectorHatText = "Hummm... Let me think..."
+            selectorHatImage = "casashp"
+        }else if counter == 2 {
+            selectorHatText = "Hufflepuff!!!"
+            selectorHatImage = "lufalufa"
+        }else if counter == 3 {
+            selectorHatText = "Hummm... Let me think..."
+            selectorHatImage = "casashp"
+        }else if counter == 4 {
+            selectorHatText = "Ravenclaw!!!"
+            selectorHatImage = "corvinal"
+        }else if counter == 5 {
+            selectorHatText = "Hummm... Let me think..."
+            selectorHatImage = "casashp"
+        }else if counter == 6{
+            selectorHatText = "Slytherin!!!"
+            selectorHatImage = "sonserina"
+        }else if counter == 7 {
+            selectorHatText = "Hummm... Let me think..."
+            selectorHatImage = "casashp"
+            //counter = 0
+            //return
+        }
+        
+        if counter < 7 {
+            counter += 1
+        }else {
+            selectorHatText = "Hummm... Let me think..."
+            selectorHatImage = "casashp"
+            //counter = 0
         }
     }
 }
